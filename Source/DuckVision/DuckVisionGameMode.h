bc80207ee9087245d5bc19bb26d7886be72e0713
KimSh <kimseonghoon24@gmail.com>
2025-11-02 12:27:21 +0900

    초기프로젝트 업

---
+++ Source/DuckVision/DuckVisionGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DuckVisionGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ADuckVisionGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ADuckVisionGameMode();
+};
+
+
+
